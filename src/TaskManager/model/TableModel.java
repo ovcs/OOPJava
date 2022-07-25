66672bdc4c100bd068b0433febb982abbc1a86ed
OvcS <gerkules007@yahoo.com>
2022-07-25 11:12:44 +0300

    fix TableModel

--- src/TaskManager/model/TableModel.java
+++ src/TaskManager/model/TableModel.java
@@ -2,16 +2,16 @@ package TaskManager.model;
 
 import java.util.*;
 
-public interface TableModel<R, T> {
-                   void addDefault(T e);
-                   void add(R r, T e);
-                   void create();
-                   void changeOrder(Comparator<T> comparator);
-                      R get(T e);
-                 Set<T> getIfContains(Object o);
-                 Map<R, T> getTable();
-                 Set<T> getAll();
-                   void load(Map<R, T> loadTable);
-                   void replace(T e, R newR);
-                   void remove(T e);
+public interface TableModel<C, T> {
+    void addDefault(T e);
+    void add(C column, T e);
+    void create();
+    void changeOrder(C column, Comparator<T> comparator);
+    C get(T e);
+    Set<T> getIfContains(Object o);
+    Set<T> getAll();
+    void load(Map<C, T> table);
+    void replace(T e, C toNewC);
+    void remove(T e);
+    Map<C, T> save();
 }
